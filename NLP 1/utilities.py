472d667eae21592973995bb58a632880992e2872
wlouhichi <wafaalouhichi@gmail.com>
2020-05-19 05:17:24 +0000

    Reduce code

---
+++ NLP 1/utilities.py
@@ -0,0 +1,100 @@
+### utility functions for the summer camp
+import matplotlib.pyplot as plt
+
+DEF_FIGLEN = 7
+DEF_FIGSIZE = (DEF_FIGLEN, DEF_FIGLEN)
+
+def figure(figsize=DEF_FIGSIZE):
+    return plt.figure(figsize=figsize)
+
+def new_blank_plot(ax=None, xlim=(-2, 2), ylim=(-2, 2), axis_color='gray', title=''):
+    if ax is None:
+        ax = plt.gca()
+    else:
+        plt.sca(ax)
+    ax.axis('equal')
+    if xlim is not None: ax.set_xlim(xlim[0], xlim[1])
+    if ylim is not None: ax.set_ylim(ylim[0], ylim[1])
+    if axis_color is not None:
+        ax.axhline(color=axis_color)
+        ax.axvline(color=axis_color)
+    if title is not None:
+        ax.set_title(title)
+    return ax
+
+def vector(*elems, dim=None):
+    """Exercise: What does this function do?"""
+    if dim is not None:
+        if len(elems) > 0:
+            assert dim == len(elems), "Number of supplied elements differs from the requested dimension."
+        else: # No supplied elements
+            elems = [0.0] * dim
+    return tuple(elems)
+
+
+def draw_vector2d(v, ax=None, origin=(0, 0), width=0.08, color='black', alpha=1.0,
+                  **kw_args):
+    assert len(v) == 2, "Input vector must be two-dimensional."
+    if ax is None:
+        ax = plt.gca()
+    ax.arrow(origin[0], origin[1], v[0], v[1],
+             width=width,
+             facecolor=color,
+             edgecolor='white',
+             alpha=alpha,
+             length_includes_head=True,
+             **kw_args);
+    
+def draw_label2d(p, label, coords=False, ax=None, fontsize=14,
+                 dp=(0.0, 0.1), horizontalalignment='center', verticalalignment='bottom',
+                 **kw_args):
+    assert len(p) == 2, "Position must be 2-D."
+    if ax is None: ax = plt.gca()
+    text = '{}'.format(label)
+    if coords:
+        text += ' = ({}, {})'.format(p[0], p[1])
+    ax.text(p[0]+dp[0], p[1]+dp[1], text,
+            fontsize=fontsize,
+            horizontalalignment=horizontalalignment,
+            verticalalignment=verticalalignment,
+            **kw_args)
+
+
+###### Code for the CNN model using Keras ##############
+import keras
+from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Activation, Conv2D, Input, Embedding, Reshape, MaxPool2D, Concatenate, Flatten, Dropout, Dense, Conv1D
+from keras.layers import MaxPool1D
+from keras.models import Model, load_model
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
+
+def CNN_Model(word_index=None, embedding_dim=None,embedding_matrix=None, num_filters=None, filter_sizes=None, max_sequence_length=None, drop=None):
+    embedding_layer = Embedding(len(word_index) + 1,
+                            embedding_dim,
+                            weights=[embedding_matrix],
+                            input_length=max_sequence_length,
+                            trainable=False)
+    inputs = Input(shape=(max_sequence_length,), dtype='int32')
+    embedding = embedding_layer(inputs)
+    reshape = Reshape((max_sequence_length,embedding_dim,1))(embedding)
+
+    conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+    conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+    conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+    maxpool_0 = MaxPool2D(pool_size=(max_sequence_length - filter_sizes[0] + 1, 1), strides=(1,1), padding='valid')(conv_0)
+    maxpool_1 = MaxPool2D(pool_size=(max_sequence_length - filter_sizes[1] + 1, 1), strides=(1,1), padding='valid')(conv_1)
+    maxpool_2 = MaxPool2D(pool_size=(max_sequence_length - filter_sizes[2] + 1, 1), strides=(1,1), padding='valid')(conv_2)
+    concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
+    flatten = Flatten()(concatenated_tensor)
+    dropout = Dropout(drop)(flatten)
+    output = Dense(1,activation = 'sigmoid')(dropout)
+    model = Model(inputs=inputs, outputs=output)
+    model.compile(optimizer=Adam(lr = 1e-2), loss='binary_crossentropy', metrics=['accuracy'])
+    return model
+
+
+
+
